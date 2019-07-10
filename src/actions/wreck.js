6ad3ba4f8c1ee14483b18a2837b3af17a07b492b
tallybee <talbondareva@gmail.com>
2019-07-10 12:08:00 +0200

    blocks for the main function

--- src/actions/wreck.js
+++ src/actions/wreck.js
@@ -6,9 +6,10 @@ export const WRECK_CHECKED = " WRECK_CHECKED";
 
 // picks the object from json data that is the pure hashtag
 const correctItem = (items, location) => {
-  console.log(items, location)
   return items.find(item => {
-    return item.link.includes(`https://www.instagram.com/explore/tags/${location}`);
+    return item.link.includes(
+      `https://www.instagram.com/explore/tags/${location}`
+    );
   });
 };
 
@@ -23,30 +24,44 @@ const stringToNumber = string => {
   const firstNumber = parseFloat(string, 10);
   switch (firstLetter) {
     case "k":
-        return firstNumber * 1000;
+      return firstNumber * 1000;
     case "m":
       return firstNumber * 1000000;
     case "b":
-      return firstNumber * 1000000000
+      return firstNumber * 1000000000;
     default:
-      return firstNumber
+      return firstNumber;
   }
 };
+
+const populationDensity = location => {
+  request
+    .get(`https://restcountries.eu/rest/v2/capital/${location}?fields=population;area`)
+    .then(response => {
+      const population = response.body[0].population
+      const area = response.body[0].area
+      return(population / area)
+    })
+}
 
 // gets number of times location was used as hashtag
 export const checkWreck = location => dispatch => {
   const formattedLocation = formatLocation(location);
+
   request
     .get(
       `https://www.googleapis.com/customsearch/v1?q=${formattedLocation}&cx=007962097164152155274:_9jk37_iaew&key=${API_KEY}`
     )
-    .then(result => { // picks out the object that contains the pure hashtag
+    .then(result => {
+      // picks out the object that contains the pure hashtag
       return correctItem(result.body.items, formattedLocation);
     })
-    .then(result => { // takes string with value, converts into number
-      return stringToNumber(result.snippet)})
-    .then(result => // dispatches it into redux store 
-      dispatch(wreckChecked(result))) 
+    .then(result => {
+      // takes string with the value, converts into number
+      return stringToNumber(result.snippet);
+    })
+    .then((result // dispatches it into redux store
+    ) => dispatch(wreckChecked(result)))
     .catch(console.error);
 };
 
